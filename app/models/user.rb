54b6ba9cd930055ddebf51e4d007eecdf2843446
Jason Shen <sikajs@gmail.com>
2013-12-31 15:10:01 +0700

    Finished until 10.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   has_secure_password
   #before_save { self.email = email.downcase }
   before_save { email.downcase! }
